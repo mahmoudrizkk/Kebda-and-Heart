6a0d78491d976e466e18b10911f3208ab4758407
Rizk <mahmoudmohd74@gmail.com>
2025-06-18 12:21:35 +0300

    qweqw

--- main.py
+++ main.py
@@ -565,9 +565,10 @@ def main():
         # Step 3: Send to server and show response
         try:
             # http://shatat-ue.runasp.net/api/Devices/LiverAndHeart?type=1&weight=21&machineId=1
+            # http://shatat-ue.runasp.net/api/Devices/LiverAndHeart?type=1&weight=12&machineId=1
             # url = f"http://shatat-ue.runasp.net/api/Devices/LiverAndHeart?type={selected_type}&weight={received_weight}&machineId=1"
-            url = f"http://shatat-ue.runasp.net/api/Devices/LiverAndHeart?type={selected_type}&weight=14&machineId=1"
-            response = urequests.post(url)
+            url = f"http://shatat-ue.runasp.net/api/Devices/LiverAndHeart?type=1&weight={received_weight}&machineId=1"
+            response = urequests.request("POST",url)
             response_text = response.text
             response.close()
 
